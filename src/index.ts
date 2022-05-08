0e4f65b27bcf0bb22db23908a678a37b7e1502e1
Zemheri25 <omerzemheri14@gmail.com>
2022-05-08 20:03:18 +0300

    Exercises Added

---
+++ src/index.ts
@@ -0,0 +1,68 @@
+// let myname : string | number = "ömer";
+
+// function change() {
+//     myname = "bahar",
+//     myname = 25
+// };
+
+// function check() {
+//     if(typeof myname === "string") {
+//        myname.replace("ömer", "bahar")
+//     }
+
+// }
+
+// let myObject: {
+//   name: string;
+//   surname: string;
+//   age: 28;
+// } = {
+//   name: "ömer",
+//   surname: "zemheri",
+//   age: 28,
+// };
+
+// myObject.name = "Bahar";
+
+
+// const myArray : (string |boolean | number)[] = [true, 2, 3, "ömer"];
+
+
+// console.log(myArray);
+// console.log("bahar")
+
+// const myArray : [string, string, number] = ["Ömer", "Zemheri", 25];
+
+// myArray[2].toFixed();
+// myArray[0] = "Bahar";
+
+// console.log(myArray);
+
+
+// function F(): number {
+//     return 25
+// };
+
+// const c = F();
+// console.log(c)
+
+
+// function LogPerson (name : string, surname : string, age : number) {
+//     return  {name, surname, age}
+    
+// };
+
+
+// console.log(LogPerson("Ömer", "Zemheri", 28));
+
+
+
+// const myFunction = (name: string, number : number) => {
+//     for(let i = 0; i < number; i++) {
+//         console.log(name)
+//     }
+// };
+
+
+// myFunction("Ömer", 10);
+
